37681dafda137cd790ff0a4605032564416f7471
rmanky <rmankaryous12@gmail.com>
2019-03-18 17:23:23 -0400

    Update question1.py

--- question1.py
+++ question1.py
@@ -5,7 +5,7 @@ arg = sys.argv[1]
 n = 100000
 
 def timeDiff (initTime):
-    print("Total running time for Part 1: ",
+    print("Total running time: ",
           int((time.time() - initTime) * 1000), " milliseconds")
 
 def part1 ():
@@ -24,7 +24,13 @@ def part2 ():
     timeDiff(initTime)
 
 def part3 ():
-    print("This is part 3")
+    sum = 0
+    initTime = time.time()
+    for x in range(n):
+        for y in range(n):
+            for z in range(n):
+                sum += 1
+    timeDiff(initTime)
 
 Questions = {
     "1" : part1,
